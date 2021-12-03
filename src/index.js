4210d371be833d00910151b5b4c15f5772862d96
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2021-12-02 20:43:59 -0800

    creating random nums

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDom from "react-dom";
 import getRandomNums from "./randomApi/randomApi";
 const App = () => {
+    const [nums, setNums] = useState([]);
+
+    const randomNums = async () => {
+        try {
+            const response = await getRandomNums();
+            setNums(response);
+        }
+        catch (error) {
+            console.log(error);
+        }
+
+    }
+    useEffect(() => {
+        randomNums();
+    }, [])
     return (
         <>
             <h1>hello world</h1>
+            {
+                nums.map((num, id) => {
+                    return (
+                        <div key={id}>
+                            <h1>{num}</h1>
+                        </div>
+                    )
+                })
+            }
         </>
     )
 }
--- src/randomApi/randomApi.js
+++ src/randomApi/randomApi.js
@@ -1,13 +1,17 @@
 import axios from "axios";
 const API_URL = "https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new"
-// https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new
+
 const getRandomNums = async () => {
     try {
         const response = await axios.get(API_URL);
-        console.log(response.data)
-        const data = await JSON.parse(response)
-        console.log(data);
-        return data;
+        const data = response.data.split("\n")
+        const result = [];
+        for (let i = 0; i < data.length; i++) {
+            if (data[i] !== "") {
+                result.push(data[i]);
+            }
+        }
+        return result;
     } catch (error) {
         console.log(error);
     }
