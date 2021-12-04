97ac35865b7711dae2c98def4d5d7efc336471f3
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2021-12-04 11:35:15 -0800

    find a way to keep all of user selections

--- src/components/Hints.js
+++ src/components/Hints.js
@@ -2,35 +2,44 @@ import React, { useContext } from "react";
 import { UserContext } from "../context/UserContext";
 const Hints = () => {
     const { hintNumbers, sequence } = useContext(UserContext);
-
-    console.log("HINTNUMS IN HINTS.JS", hintNumbers)
-
     const calculateHints = (numbers) => {
         const hints = [];
-        const dupCheck = [];
 
         numbers.forEach((num, idx) => {
             if (sequence[idx] === num) {
                 hints.push("full");
-                dupCheck.push(num);
-            }
-            if (!dupCheck.includes(num) && sequence.includes(num)) {
+            } else if (sequence.includes(num)) {
                 hints.push("half")
+            } else {
+                hints.push("empty")
             }
         })
 
         return hints;
     }
+    const hintsArray = calculateHints(hintNumbers);
+    console.log("ORIGINAL",hintsArray)
 
+    const shuffleHints = (hintsArr) => {
+        for (let i = hintsArr.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            let temp = hintsArr[i];
+            hintsArr[i] = hintsArr[j];
+            hintsArr[j] = temp;
+        }
+        return hintsArr;
+    }
+
+    const shuffledHintArr = shuffleHints(hintsArray);
+    console.log("SHUFFLED", shuffledHintArr);
     return (
         <>
             <h4>Hints</h4>
             {
-                calculateHints(hintNumbers).map((hintNum, id) => (
-                    <div className="hints" key={id}>
-                        {hintNum}
+                shuffledHintArr.length !== 0 ? shuffledHintArr.map((hintNumClass, id) => (
+                    <div className={hintNumClass} key={id}>
                     </div>
-                ))
+                )) : ""
             }
         </>
     )
