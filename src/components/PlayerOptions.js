e5b6256fac5cea3bcf8b93b094e9d6e5674b89c5
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2021-12-06 20:59:37 -0800

    fixed button issue for event.currentTarget.value in PlayerOption.js

--- src/components/PlayerOptions.js
+++ src/components/PlayerOptions.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { UserContext } from "../context/UserContext";
-import { Button, BottomNavigation, Paper } from "@mui/material";
+import { Button, BottomNavigation, Paper, Typography, BottomNavigationAction } from "@mui/material";
+import { makeStyles } from "@mui/styles"
 
 const PlayerOptions = () => {
     const { setSelectedNumbers, starterNums } = useContext(UserContext);
@@ -12,21 +13,23 @@ const PlayerOptions = () => {
     }
 
     const onClickHandler = (event) => {
-        const selectedNum = event.target.value
+        const selectedNum = event.currentTarget.value;
         addSelectedNums(selectedNum);
     };
 
     return (
-        <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, backgroundColor: "rgb(83 82 84)" }} elevation={3}>
-            <BottomNavigation sx={{ m: 3,}} sx={{ backgroundColor: "rgb(83 82 84)" }}>
+        <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, backgroundColor: "rgb(83 82 84)" }}>
                 {
                     starterNums.map((num, id) => (
-                        <Button variant="outlined" value={num} onClick={onClickHandler} key={id}>
-                            {num}
+                        <Button
+                            variant="contained"
+                            value={num}
+                            onClick={onClickHandler}
+                            key={id}
+                            ><Typography>{num}</Typography>
                         </Button>
                     ))
                 }
-            </BottomNavigation>
         </Paper>
 
     )
